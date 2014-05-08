d96873c601eb09da59c7263db11d74f279b475df
Naixor <hongyesoftware@gmail.com>
2014-05-08 20:59:55 +0800

    share users

--- realtimeDemo/index.js
+++ realtimeDemo/index.js
@@ -5,7 +5,7 @@
   app = angular.module('Realtime', []);
 
   app.controller('realtimeController', function($scope) {
-    var Docs, appReady, edit1, edit2;
+    var Docs, appReady, edit1, edit2, fileLoaded;
     Docs = {};
     edit1 = document.getElementById('editer1');
     edit2 = document.getElementById('editer2');
@@ -15,6 +15,7 @@
     $scope.isLoaded = false;
     $scope.realtimefile = {};
     $scope.femail = "";
+    $scope.users = [];
     $scope.authorize = function() {
       var objAuth;
       objAuth = {
@@ -42,14 +43,46 @@
       });
       return window.c_file.id;
     };
+    $scope.getShareUser = function() {
+      return Nimbus.Client.GDrive.get_shared_users_real(function(users) {
+        var user, _i, _len;
+        console.log(users);
+        for (_i = 0, _len = users.length; _i < _len; _i++) {
+          user = users[_i];
+          $scope.users.push(user);
+        }
+        return $scope.$apply();
+      });
+    };
+    fileLoaded = function(doc) {
+      var string;
+      string = doc.getModel().getRoot().get('todo');
+      edit2.value = string.get("text");
+      return gapi.drive.realtime.databinding.bindString(string.get("text", edit2));
+    };
     appReady = function() {
       if (Nimbus.Client.GDrive.check_auth) {
+        window.showChangeText = edit2;
         $scope.authorized = true;
         console.log("c_file.id:" + window.c_file.id);
         $scope.realtimeData = window.todo.get("text");
         Nimbus.Model.Realtime.set_objectchanged_callback(function(e) {
           console.log("changed!");
           return edit2.value = window.todo.get("text");
+        });
+        Nimbus.Client.GDrive.get_shared_users_real(function(users) {
+          var user, _i, _len;
+          console.log(users);
+          for (_i = 0, _len = users.length; _i < _len; _i++) {
+            user = users[_i];
+            $scope.users.push(user);
+          }
+          return $scope.$apply();
+        });
+        Nimbus.Client.GDrive.getFile(c_file.id, function(para) {
+          return Nimbus.Client.GDrive.readFile(para.selfLink, function(readFile) {
+            return window.readFile = readFile;
+          });
         });
         edit1.onkeyup = function(e) {
           return window.todo.set("text", edit1.value);
@@ -80,11 +113,7 @@
       },
       "synchronous": true
     };
-    Nimbus.Auth.setup(objAuth);
-    Nimbus.Auth.authorize('GDrive');
-    return Nimbus.Auth.authrized_callback = function() {
-      return console.log("Authorized sucess!");
-    };
+    return Nimbus.Auth.setup(objAuth);
   };
 
 }).call(this);
